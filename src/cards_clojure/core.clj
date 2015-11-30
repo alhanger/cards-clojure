31fdf1f64ab0ecc4bd226f0bed41811e873d1850
alhanger <hanger129@gmail.com>
2015-11-30 15:18:11 -0500

    Add functions to test for straight-flush, straight, 4 and 3 of a kind, and two-pair

--- src/cards_clojure/core.clj
+++ src/cards_clojure/core.clj
@@ -3,13 +3,13 @@
 
 (def suits [:club :spade :heart :diamond])
 (def ranks (range 1 14))
-(def rank-names {1 :ace, 11 :jack, 12 :queen, 13 :king})
+;(def rank-names {1 :ace, 11 :jack, 12 :queen, 13 :king})
 
 (defn create-deck []
   (set (for [suit suits
              rank ranks]
          {:suit suit
-          :rank (get rank-names rank rank)})))
+          :rank rank})))
 
 (defn create-hands [deck]
   (set (for [c1 deck
@@ -20,6 +20,26 @@
 
 (defn flush? [hand]
   (= 1 (count (set (map :suit hand)))))
+
+(defn straight? [hand]
+  (let [sorted (sort (map :rank hand))
+        min-value (first sorted)]
+    (= sorted
+       (take 4
+             (iterate inc min-value)))))
+
+(defn straight-flush? [hand]
+  (and (flush? hand)
+       (straight? hand)))
+
+(defn four-of-a-kind? [hand]
+  (= 1 (count (set (map :rank hand)))))
+
+(defn three-of-a-kind? [hand]
+  (== (apply max (vals (frequencies (map :rank hand)))) 3))
+
+(defn two-pair? [hand]
+  (== (apply max (vals (frequencies (map :rank hand)))) 2))
 
 (defn -main [& args]
   (time (let [deck (create-deck)
--- test/cards_clojure/core_test.clj
+++ test/cards_clojure/core_test.clj
@@ -7,20 +7,91 @@
               {:suit :club
                :rank 3}
               {:suit :club
-               :rank :queen}
+               :rank 12}
               {:suit :club
-               :rank :king}})
+               :rank 13}})
 
 (def hand-2 #{{:suit :spade
                :rank 2}
               {:suit :club
                :rank 3}
               {:suit :club
-               :rank :queen}
+               :rank 12}
               {:suit :club
-               :rank :king}})
+               :rank 13}})
+
+(def hand-3 #{{:suit :spade
+               :rank 2}
+              {:suit :club
+               :rank 2}
+              {:suit :diamond
+               :rank 2}
+              {:suit :heart
+               :rank 2}})
+
+(def hand-4 #{{:suit :spade
+               :rank 2}
+              {:suit :club
+               :rank 2}
+              {:suit :diamond
+               :rank 2}
+              {:suit :heart
+               :rank 3}})
+
+(def hand-5 #{{:suit :spade
+               :rank 2}
+              {:suit :club
+               :rank 2}
+              {:suit :diamond
+               :rank 3}
+              {:suit :heart
+               :rank 3}})
+
+(def hand-6 #{{:suit :spade
+               :rank 2}
+              {:suit :club
+               :rank 3}
+              {:suit :diamond
+               :rank 4}
+              {:suit :heart
+               :rank 5}})
+
+(def hand-7 #{{:suit :spade
+               :rank 2}
+              {:suit :spade
+               :rank 3}
+              {:suit :spade
+               :rank 4}
+              {:suit :spade
+               :rank 5}})
 
 (deftest flush?-test
   (testing "flush? returns true if the hand is a flush"
     (is (= true (flush? hand-1)))
     (is (= false (flush? hand-2)))))
+
+(deftest four-of-a-kind?-test
+  (testing "four-of-a-kind? returns true if the hand has four cards with the same rank"
+    (is (= true (four-of-a-kind? hand-3)))
+    (is (= false (four-of-a-kind? hand-2)))))
+
+(deftest three-of-a-kind?-test
+  (testing "three-of-a-kind? returns true if the hand has three cards with the same rank"
+    (is (= true (three-of-a-kind? hand-4)))
+    (is (= false (three-of-a-kind? hand-3)))
+    (is (= false (three-of-a-kind? hand-2)))))
+
+(deftest two-pair?-test
+  (testing "two-pair? returns true if the hand has two pairs with the same rank"
+    (is (= true (two-pair? hand-5)))
+    (is (= false (two-pair? hand-4)))))
+
+(deftest straight?-test
+  (testing "straight? returns true if the rank of the cards are in sequential order"
+    (is (= true (straight? hand-6)))
+    (is (= false (straight? hand-5)))))
+
+(deftest straight-flush?-test
+  (testing "straight-flush? returns true if the suit of all the cards is the same and the ranks are in sequential order"
+    (is (= true (straight-flush? hand-7)))
+    (is (= false (straight-flush? hand-6)))))
